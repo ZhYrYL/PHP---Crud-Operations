055f42a9427dca3a8d6b5b56b73dd242dcd1b5cf
Zhy <zhyryllposadas@gmail.com>
2023-02-22 22:10:34 +0800

    Can now search records and display it in the form

--- grades.php
+++ grades.php
@@ -51,6 +51,27 @@
             padding: 10px;
             border: 1px solid;
         }
+        .search-txt{
+            border-radius: 3px;
+            height: 25px;
+            border: 1px solid gray;
+        }
+        .search-btn{
+            height: 30px;
+            cursor: pointer;
+            background-color: #9CFF2E;
+            color: white;
+            font-size: 16px;
+            font-weight: bold;
+            border: none;
+            text-shadow: 0 0 1px black;
+            border-radius: 3px;
+        }
+        .search-btn:hover{
+            transition: 0.3s;
+            background-color: #38E54D;
+            color: gold;
+        }
     </style>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -97,6 +118,12 @@
         <form method="post">
             <table class="table-container">
                 <tr>
+                    <td colspan="7" style="text-align: left">
+                        <input type="text" name="search-txt" class="search-txt">
+                        <input type="submit" name="search-btn" class="search-btn" value="Search"> 
+                    </td>
+                </tr>
+                <tr style="font-weight: bold;">
                     <td>Student ID</td>
                     <td>First Name</td>
                     <td>Last Name</td>
@@ -108,7 +135,13 @@
 
                 <?php
                     include 'db/connection.php';
-                    $select = mysqli_query($connection, "SELECT * FROM grades");
+
+                    if(isset($_POST['search-btn'])){
+                        $select = mysqli_query($connection, "SELECT * FROM grades where student_id LIKE '%".$_POST['search-txt']."%' or fname LIKE '%".$_POST['search-txt']."%' 
+                        or lname LIKE '%".$_POST['search-txt']."%' or age LIKE '%".$_POST['search-txt']."%' or section LIKE '%".$_POST['search-txt']."%' or mname LIKE '%".$_POST['search-txt']."%'");
+                    }else{
+                        $select = mysqli_query($connection, "SELECT * FROM grades");
+                    }
                     while($result = mysqli_fetch_object($select)){
                 ?>
                 
